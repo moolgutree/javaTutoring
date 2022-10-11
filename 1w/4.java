3ff0a6d147f62e7208b5aa4884c086bab4e8df95
moolgutree <85077262+moolgutree@users.noreply.github.com>
2022-10-11 19:11:02 +0900

    Create 4.java

---
+++ 1w/4.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+    
+    
+    for (int j=0; j <= 9; j++) 
+      if (j % 2 != 0)
+        System.out.println("5 + " +j+ " = " + 5*j);      
+	}
+}
