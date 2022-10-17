b71fe120e82e90c4f1b4d4b6ca8a4bea5d80f710
moolgutree <85077262+moolgutree@users.noreply.github.com>
2022-10-17 21:55:36 +0900

    Create 문제6.java

---
+++ 2w/문제6.java
@@ -0,0 +1,16 @@
+// tip: each public class is put in its own file
+public class Main
+{
+    // tip: arguments are passed via the field below this editor
+    public static void main(String[] args)
+    {
+      System.out.println("일  월  화  수  목  금  토")
+      System.out.println("                    ")
+      for (int A = 1  ; A <= 31 ; A++) {
+        System.out.println("                      A")
+        }
+      
+      
+      System.out.println("Hello, world!");
+    }
+}
